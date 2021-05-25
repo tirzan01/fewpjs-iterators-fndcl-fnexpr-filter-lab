d7f7df557f4d4455e99a00210e7d3c80025a6b17
thomas <thomasmonta98@gmail.com>
2021-05-25 21:08:02 +1000

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(arrayDriver, string){
+    return arrayDriver.filter(driverName=>string.toLowerCase() === driverName.toLowerCase())
+   
+}
+
+function fuzzyMatch(arrayDriver, string){
+    return arrayDriver.filter(driver=>{return string === driver.slice(0, string.length)})   
+}
+
+function matchName(arrayDriver, string){
+    const nameMatching = arrayDriver.filter(driver=>{return string===driver.name})
+    return nameMatching
+}
